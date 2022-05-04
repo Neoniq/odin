e25603844980a61f962ba15dd440339dad7db5d2
Neoniq <demon9836@gmail.com>
2022-05-04 18:22:00 +0300

    mouse hover rgb effect

--- ETCH-A-SKETCH/js.js
+++ ETCH-A-SKETCH/js.js
@@ -8,20 +8,27 @@ function setupGrid(size) {
   grid.replaceChildren()
   grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`
   grid.style.gridTemplateRows = `repeat(${size}, 1fr)`
-  console.log('SetupGrid:', size + "=" + (size * size))
+  console.log('Grid: ', size + ' => ' + size * size)
 
   for (let i = 0; i < size * size; i++) {
     const gridElement = document.createElement('div')
     gridElement.classList.add('grid-element')
     gridElement.addEventListener('mouseover', changeColor)
-    gridElement.addEventListener('mousedown', changeColor)
+    // gridElement.addEventListener('mousedown', changeColor)
     grid.appendChild(gridElement)
-    console.log(i);
+    // console.log(i);
   }
 }
+
 setupGrid(16)
 
-function changeColor(params) {}
+function changeColor(el) {
+  el.target.style.backgroundColor = `rgb(${rgbGen()}, ${rgbGen()}, ${rgbGen()})`
+}
+
+function rgbGen() {
+  return (Math.random() * 256).toFixed()
+}
 
 gridSlider.addEventListener('change', (e) => {
   // console.log(e, 'asd')
